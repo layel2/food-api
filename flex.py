d876f6849248fe26f554d408a8235660f1d1a367
layel2 <chaos_layyer@live.com>
2020-10-19 23:59:29 +0700

    fix-fplx

--- flex.py
+++ flex.py
@@ -263,6 +263,131 @@ def get_flex():
                                     }
                                 ]
                             }
+                        },{
+                            "type": "bubble",
+                            "hero": {
+                                "type": "image",
+                                "url": "https://static.posttoday.com/media/content/2018/10/09/7E68135B4E03435881B219B331FB9169.jpg",
+                                "size": "full",
+                                "aspectRatio": "20:13",
+                                "aspectMode": "cover",
+                                "backgroundColor": "#FFFFFFFF",
+                                "action": {
+                                    "type": "uri",
+                                    "label": "Line",
+                                    "uri": "https://linecorp.com/"
+                                }
+                            },
+                            "body": {
+                                "type": "box",
+                                "layout": "vertical",
+                                "contents": [
+                                    {
+                                        "type": "text",
+                                        "text": "ส้มตำป้าส้ม",
+                                        "weight": "bold",
+                                        "size": "xl",
+                                        "contents": []
+                                    },
+                                    {
+                                        "type": "box",
+                                        "layout": "baseline",
+                                        "margin": "md",
+                                        "contents": [
+                                            {
+                                                "type": "icon",
+                                                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+                                                "size": "sm"
+                                            },
+                                            {
+                                                "type": "icon",
+                                                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+                                                "size": "sm"
+                                            },
+                                            {
+                                                "type": "icon",
+                                                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+                                                "size": "sm"
+                                            },
+                                            {
+                                                "type": "icon",
+                                                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+                                                "size": "sm"
+                                            },
+                                            {
+                                                "type": "icon",
+                                                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png",
+                                                "size": "sm"
+                                            },
+                                            {
+                                                "type": "text",
+                                                "text": "4.0",
+                                                "size": "sm",
+                                                "color": "#999999",
+                                                "flex": 0,
+                                                "margin": "md",
+                                                "contents": []
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        "type": "box",
+                                        "layout": "vertical",
+                                        "spacing": "sm",
+                                        "margin": "lg",
+                                        "contents": [
+                                            {
+                                                "type": "box",
+                                                "layout": "baseline",
+                                                "spacing": "sm",
+                                                "contents": [
+                                                    {
+                                                        "type": "text",
+                                                        "text": "ที่อยู่",
+                                                        "size": "sm",
+                                                        "color": "#AAAAAA",
+                                                        "flex": 1,
+                                                        "contents": []
+                                                    },
+                                                    {
+                                                        "type": "text",
+                                                        "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
+                                                        "size": "sm",
+                                                        "color": "#666666",
+                                                        "flex": 5,
+                                                        "wrap": True,
+                                                        "contents": []
+                                                    }
+                                                ]
+                                            },
+                                            {
+                                                "type": "box",
+                                                "layout": "baseline",
+                                                "spacing": "sm",
+                                                "contents": [
+                                                    {
+                                                        "type": "text",
+                                                        "text": "เวลา",
+                                                        "size": "sm",
+                                                        "color": "#AAAAAA",
+                                                        "flex": 1,
+                                                        "contents": []
+                                                    },
+                                                    {
+                                                        "type": "text",
+                                                        "text": "10:00 - 23:00",
+                                                        "size": "sm",
+                                                        "color": "#666666",
+                                                        "flex": 5,
+                                                        "wrap": True,
+                                                        "contents": []
+                                                    }
+                                                ]
+                                            }
+                                        ]
+                                    }
+                                ]
+                            }
                         }
                     ]
                 }
