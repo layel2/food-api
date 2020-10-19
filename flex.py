f531294868b0d09881d772ea756af4ad1f8ae3ee
layel2 <chaos_layyer@live.com>
2020-10-20 01:35:11 +0700

    update

--- flex.py
+++ flex.py
@@ -102,7 +102,7 @@ def getFlexcontent(data):
                                 ]
                             }
                         }
-
+    return content
 def getStar(n_star):
     star = {
             "type": "icon",
