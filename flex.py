966e30ac0c548186c06af8ab194f2dcb4e8c3e43
layel2 <chaos_layyer@live.com>
2020-10-20 01:31:23 +0700

    update

--- flex.py
+++ flex.py
@@ -14,8 +14,7 @@ def get_flex(data,n_res):
                 "altText": "this is a flex message",
                 "contents": {
                     "type": "carousel",
-                    "contents": [
-                    ]
+                    "contents": main_contents
                 }
             }
         ]
